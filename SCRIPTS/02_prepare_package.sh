5b4a5e49b9942f39cb046bf4fb116f15df4a7875
nicksun98 <44309749+nicksun98@users.noreply.github.com>
2020-11-17 19:42:57 +0800

    Driver

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -4,8 +4,8 @@ clear
 #update r8152 driver
 #wget -O- https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3178.patch | patch -p1
 #wget -O- https://github.com/project-openwrt/openwrt/commit/d8df86130d172b3ce262d2744e2ddd2a6eed5f50.patch | patch -p1
-#svn co https://github.com/project-openwrt/openwrt/branches/master/package/ctcgfw/r8152 package/new/r8152
-#sed -i '/rtl8152/d' ./target/linux/rockchip/image/armv8.mk
+svn co https://github.com/project-openwrt/openwrt/branches/master/package/ctcgfw/r8152 package/new/r8152
+sed -i '/rtl8152/d' ./target/linux/rockchip/image/armv8.mk
 
 notExce(){ 
 #blocktrron.git 
